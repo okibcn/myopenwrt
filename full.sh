43332009d281830a1cd2b3799feed26a3779e172
okibcn <okibcn@gmail.com>
2022-03-28 00:40:08 -0700

    fix REPO error

--- full.sh
+++ full.sh
@@ -156,8 +156,8 @@ set_R6020() {
     ARCH="mt76x8"
     DIFF_PACKAGES="nano-plus htop iperf3 luci wpad-mesh-wolfssl -wpad-basic-wolfssl luci-app-vnstat2 luci-app-nlbwmon"
     DIFF_CONFIG=R6020.config
-    REPO=
-    BRANCH="openwrt-22.03"
+    REPO=https://github.com/openwrt/openwrt
+    BRANCH=openwrt-22.03
     dl_ow $DIR $REPO $BRANCH
     set_packages "$DIFF_PACKAGES"
 #    add_argon
